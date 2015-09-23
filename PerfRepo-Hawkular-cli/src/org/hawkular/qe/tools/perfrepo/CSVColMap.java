128edfb518a755b1f18fef807986f852325f3b8a
Forykx <prusa.vojtech@gmail.com>
2015-09-23 17:38:23 +0200

    Configuration structure and parsing *.csv files.

---
+++ PerfRepo-Hawkular-cli/src/org/hawkular/qe/tools/perfrepo/CSVColMap.java
@@ -0,0 +1,8 @@
+package org.hawkular.qe.tools.perfrepo;
+
+import java.util.ArrayList;
+
+public class CSVColMap {
+	public String sourceCSVFilePath;
+	public ArrayList<MetricColRelation> metricColRelation;
+}
---
+++ PerfRepo-Hawkular-cli/src/org/hawkular/qe/tools/perfrepo/MetricColRelationValues.java
@@ -0,0 +1,8 @@
+package org.hawkular.qe.tools.perfrepo;
+
+import java.util.ArrayList;
+
+public class MetricColRelationValues extends MetricColRelation {
+	// values
+	public ArrayList<String> values;
+}
